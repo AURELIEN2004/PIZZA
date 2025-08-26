a03a437d5dbdac809dfb4961c7a9878b8ea2b418
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-08-26 10:08:15 +0100

    modif

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/Footer.css";
+import { FaInstagram, FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <a href="https://instagram.com"><FaInstagram /></a>
+        <a href="https://twitter.com"><FaTwitter /></a>
+        <a href="https://facebook.com"><FaFacebook /></a>
+        <a href="https://linkedin.com"><FaLinkedin /></a>
+      </div>
+      <p> &copy; 2021 pedrotechpizza.com</p>
+    </div>
+  );
+}
+
+export default Footer;
+
+
