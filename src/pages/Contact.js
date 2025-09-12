a54f7b65b78bfd97fec8d0645cbab6c551840c5b
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-09-12 23:40:41 +0100

    modif

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -16,7 +16,7 @@ function Contact() {
           <label htmlFor="name">Full Name</label>
           <input name="name" placeholder="Enter full name..." type="text" />
           <label htmlFor="email">Email</label>
-          <input name="email" placeholder="Enter email.." type="email" />
+          <input name="email" placeholder="Enter email..." type="email" />
           <label htmlFor="message">Message</label>
           <textarea
             rows="6"
@@ -24,7 +24,7 @@ function Contact() {
             name="message"
 
             required
-            
+
           ></textarea>
           <button type="submit"> Send Message</button>
         </form>
