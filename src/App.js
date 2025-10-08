6842f4bd3a0a31d1aff127ca7c686efc577198f8
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-10-08 08:40:35 +0100

    mof

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <Navbar/>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu"  element={<Menu/>} />
+        <Route path="/menu" element={<Home />} />
+        <Route path="/"  element={<Menu/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/about" element={<About />} />
       </Routes>
